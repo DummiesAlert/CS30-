a0d0c0b4c2926c29f2980d33fe3370622f46b23f
904273001 <904273001@L815WD220012.edu.cbe.ab.ca>
2022-03-29 10:11:34 -0600

    Update

--- Chapter_8/src/Vehicle_Car_Truck_Minivan/Minivan.java
+++ Chapter_8/src/Vehicle_Car_Truck_Minivan/Minivan.java
@@ -1,3 +1,16 @@
+/*
+
+Program: Vehicle.java          Last Date of this Revision: September 30, 2019
+
+Purpose: An Class Example For a Use For the Task
+
+Author: Zhuo Xi Hong, 
+School: CHHS
+Course: Computer Programming 30
+ 
+
+*/
+
 package Vehicle_Car_Truck_Minivan;
 
 public class Minivan extends Vehicle{
