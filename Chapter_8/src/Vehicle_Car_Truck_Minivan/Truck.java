0e544ff5e9438ce6878923e1e0a3e63303a3b06b
904273001 <904273001@L815WD220012.edu.cbe.ab.ca>
2022-03-30 10:43:52 -0600

    Update

--- Chapter_8/src/Vehicle_Car_Truck_Minivan/Truck.java
+++ Chapter_8/src/Vehicle_Car_Truck_Minivan/Truck.java
@@ -15,4 +15,21 @@ package Vehicle_Car_Truck_Minivan;
 
 public class Truck extends Vehicle{
 
+	public Truck(int fECity, int fEHwy, int seating, double cargo) {
+		super(fECity, fEHwy, seating, cargo);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	String cargoContainer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	String description() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
