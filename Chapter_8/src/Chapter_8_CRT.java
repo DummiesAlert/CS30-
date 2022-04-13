20a656a16bac470efe0f2ae5bad45861203a6ddc
904273001 <904273001@L815WD220012.edu.cbe.ab.ca>
2022-04-13 11:12:53 -0600

    Update

--- Chapter_8/src/Chapter_8_CRT.java
+++ Chapter_8/src/Chapter_8_CRT.java
@@ -2,7 +2,7 @@
 
 1. The has-a function is used to minimize the amount of code that would be duplicated and be used to reference an instance in a class to another class 
 or within its own class with different instances and has a composition that is a has a. Therefore a is-a is an inheritance and has a relation occuring 
-when one class inherits another attributes. (In simple terms, a has-a relationship is a class that contains a class member variables within the class 
+when one class inherits another attributes. (In simple terms, a has-a relationship is a class that contains a class member variable within the class 
 while a is-a is derived from an existing class. 
 
 For example:
@@ -26,7 +26,7 @@ public static void main(String args[]) {
 }
 
 4. An abstract class containing one or more abstract methods is required to be declared abstract, otherwise it won’t work properly. An interface with abstract 
-methods cannot be inherited in any sort or form but can be implemented by infinite amount of classes. 
+methods cannot be inherited in any sort or form but can be implemented by an infinite amount of classes. 
 
 For example:
 
@@ -53,7 +53,7 @@ c) doThat() is within “Roo” because of the methods, the interface has to be defi
 
 d) doThis(), doNow(), and doThat() is available.
 
-e) The doThis() implementation in “Roo” overrides the doThis() in “Bo”. Moreover it can confuse the code and mistaken for another method in the class. 
+e) The doThis() implementation in “Roo” overrides the doThis() in “Bo”. Moreover it can confuse the code and be mistaken for another method in the class. 
 
 f) Super(1) in “Roo” implements the value 1 to the x variable in “Bo”, which calls a base class. 
 
