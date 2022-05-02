1b219b54bf79fe9b180f69d84fc67af4dbd617b6
904273001 <904273001@L815WD220012.edu.cbe.ab.ca>
2022-05-02 11:45:52 -0600

    Update

--- Chapter_11/src/Tasks/Review_Find_And_Replace.java
+++ Chapter_11/src/Tasks/Review_Find_And_Replace.java
@@ -1,8 +1,60 @@
 package Tasks;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Review_Find_And_Replace {
 
-}
+	public static void main(String[] args) 
+	{
+		String fileName, searchText, replaceText;
+		Scanner input = new Scanner(System.in);
+		BufferedReader readFile;
+		FileReader in;
+		String replaced;
+		
+		System.out.print("Enter the Name of the File:");
+		fileName = input.nextLine();
+		
+		File textFile = new File(fileName);
+		
+		try
+		{
+			System.out.println("What Letter/Word/Phrase would you like to replace?");
+			searchText = input.nextLine();
+			System.out.println("What Letter/Word/Phrase would you like to replace it with?");
+			replaceText = input.nextLine();
+		
+			in = new FileReader(textFile);
+			readFile = new BufferedReader(in);
+			
+			replaced = readFile.readLine();
+			System.out.println(replaced);
+
+
+			System.out.println("Replacing " + searchText + " with " + replaceText);
+			System.out.println(replaced.replaceAll(searchText, replaceText));
+			
+			readFile.close();
+			in.close();
+		
+		}catch(FileNotFoundException e)
+		{
+			System.out.println("File does not exist!");
+			System.err.println("FileNotFoundException: "+ e.getMessage());
+		}
+		catch(IOException e)
+		{
+			System.out.println("Problem reading file.");
+			System.err.println("IOException: "+ e.getMessage());
+		}
+		
+		
+		
+		
+	}
+
+}	
 
 /*
 
