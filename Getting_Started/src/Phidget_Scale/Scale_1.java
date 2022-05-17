90c77337a2de1b7cce1fc11084e5a6f4594f85c3
904273001 <904273001@L815WD220012.edu.cbe.ab.ca>
2022-05-17 10:10:06 -0600

    Update

---
+++ Getting_Started/src/Phidget_Scale/Scale_1.java
@@ -0,0 +1,22 @@
+package Phidget_Scale;
+
+//Add Phidgets Library
+import com.phidget22.*;
+
+public class Scale_1 {
+	
+    public static void main(String[] args) throws Exception{
+        //Create
+        VoltageRatioInput scale = new VoltageRatioInput();
+        
+        //Open
+        scale.open(1000);
+        
+        //Use your Phidgets
+        while(true){
+            System.out.println("Offset Value: " + scale.getVoltageRatio());
+            Thread.sleep(250);
+        }
+    }    
+}
+  
